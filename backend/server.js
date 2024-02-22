a71809d4e13c6105c7066f6ce6b9f4e076d63cdf
Zhengkai Chen <z5198132@ad.unsw.edu.au>
2024-02-22 21:43:27 +0800

    Added cors in backend

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,14 @@ const port = process.env.PORT || 5000;
 const errorHandler = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const path = require("path");
+const cors = require("cors");
 
 connectDB();
 
 const app = express();
 // app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
+app.use(cors());
 app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 
