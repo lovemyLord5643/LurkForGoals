2547413517d91f04d00b7c331ac10a926d79f7b5
Zhengkai Chen <z5198132@ad.unsw.edu.au>
2024-02-22 22:23:12 +0800

    Added methods of cors

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: ["https://lurkforgoals-frontend.vercel.app"],
+    methods: ["POST", "GET"],
   })
 );
 app.use("/api/goals", require("./routes/goalRoutes"));
