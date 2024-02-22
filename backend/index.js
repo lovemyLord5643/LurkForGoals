8879f33e184d5f63c203ba598ab9188f2ecf3ae9
Zhengkai Chen <z5198132@ad.unsw.edu.au>
2024-02-22 22:25:52 +0800

    Tried again

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://lurkforgoals-frontend.vercel.app"],
+    origin: ["https://lurkforgoals-frontend.vercel.app/"],
     methods: ["POST", "GET"],
   })
 );
